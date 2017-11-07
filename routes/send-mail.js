e8c3248f76b3d0ab2e9068efe98641895e765863
dainemarshall@gmail.com <dainemarshall@gmail.com>
2017-11-07 13:14:12 -0500

    resume link minor style

--- routes/send-mail.js
+++ routes/send-mail.js
@@ -26,8 +26,8 @@ router.post('/sendMail', function(req, res) {
             console.log('errors', err.message);
             return res.redirect('/#contact');
         }
-
         res.redirect('/#contact');
+        //alert('Thank You ' + req.body.name + ', your message has been sent')
     });
 
 
@@ -39,36 +39,5 @@ router.post('/sendMail', function(req, res) {
 
 
 module.exports = router;
-/**
- * POST /contact
- * Send a contact form via Nodemailer.
- */
-exports.postContact = (req, res) => {
-  console.log(req.body);
-  req.assert('name', 'Name cannot be blank').notEmpty();
-  req.assert('email', 'Email is not valid').isEmail();
-  req.assert('message', 'Message cannot be blank').notEmpty();
 
-  const errors = req.validationErrors();
 
-  if (errors) {
-    req.flash('errors', errors);
-    return res.redirect('/#contact');
-  }
-
-  const mailOptions = {
-    to: 'dainemarshall@gmail.com',
-    from: req.body.name + req.body.email,
-    subject: 'Contact Form | The Indigo Pearl',
-    text: req.body.name + ' ' + req.body.email + ' ' + req.body.message
-  };
-
-  transporter.sendMail(mailOptions, (err) => {
-    if (err) {
-      req.flash('errors', { msg: err.message });
-      return res.redirect('/#contact');
-    }
-    req.flash('success', { msg: 'Email has been sent successfully!' });
-    res.redirect('/#contact');
-  });
-};
