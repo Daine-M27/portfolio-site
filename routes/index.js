46f194578980179688c22aa9751aa5bf136f56d8
Daine Marshall <dmarshall@thelightsource.com>
2022-06-06 11:32:48 -0400

    added math check for email form

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Daine Marshall - Web Developer' });
+router.get('/', async function(req, res, next) {
+  // let svgFilename;
+  // get captcha svg
+  // fs.promises.readdir('./captchaSVGS')
+  // .then(fileNames => {
+  //   svgFilename = fileNames[Math.floor(Math.random()*fileNames.length)]
+  //   console.log(svgFilename);
+  //   res.render('index', { title: 'Daine Marshall - Web Developer', svg: svgFilename });
+
+  // })
+  res.render('index', { title: 'Daine Marshall - Web Developer', emailStatus: ''});
 });
 
 
--- routes/send-mail.js
+++ routes/send-mail.js
@@ -5,33 +5,37 @@ const nodemailer = require('nodemailer');
 
 //----- send mail function -----//
 router.post('/sendMail', function(req, res) {
-    console.log(process.env.MAIL_USER);
-    const transporter = nodemailer.createTransport({
-        host: 'mail.privateemail.com',
-        port: 465,
-        auth: {
-            user: process.env.MAIL_USER,
-            pass: process.env.MAIL_PASS
-        },
-        secure: true
-    });
-
-
-    const mailOptions = {
-        to: 'dainemarshall@gmail.com',
-        from: process.env.MAIL_USER,
-        subject: 'Contact Form | DaineMarshall.com',
-        text: req.body.contactName + ' ' + req.body.contactEmail + ' ' + req.body.contactMessage
-    };
-
-    transporter.sendMail(mailOptions, (err, info) => {
-        if (err) {
-            console.log('errors', err.message);
-            res.render('index', { title: 'Daine Marshall - Web Developer', emailStatus: false, emailMessage: 'Sorry there was an error sending your message.  Please try again later.' });
-        }
-        res.render('index', { title: 'Daine Marshall - Web Developer', emailStatus: true, emailMessage: 'Email sent successfully, I look forward to speaking with you soon.' });
-        //alert('Thank You ' + req.body.name + ', your message has been sent')
-    });
+    console.log(req.body.svg);
+    if (req.body.svg === '4') {
+        const transporter = nodemailer.createTransport({
+            host: 'mail.privateemail.com',
+            port: 465,
+            auth: {
+                user: process.env.MAIL_USER,
+                pass: process.env.MAIL_PASS
+            },
+            secure: true
+        });
+          
+        const mailOptions = {
+            to: 'dainemarshall@gmail.com',
+            from: process.env.MAIL_USER,
+            subject: 'Contact Form | DaineMarshall.com',
+            text: req.body.contactName + ' ' + req.body.contactEmail + ' ' + req.body.contactMessage
+        };
+    
+        transporter.sendMail(mailOptions, (err, info) => {
+            if (err) {
+                console.log('errors', err.message);
+                res.render('index', { title: 'Daine Marshall - Web Developer', emailStatus: 'false', emailMessage: 'Sorry there was an error sending your message.  Please try again later.' });
+            }
+            res.render('index', { title: 'Daine Marshall - Web Developer', emailStatus: 'true', emailMessage: 'Email sent successfully, I look forward to speaking with you soon.' });
+            //alert('Thank You ' + req.body.name + ', your message has been sent')
+        });
+        
+    } else {
+        res.render('index', { title: 'Daine Marshall - Web Developer', emailStatus: 'false', emailMessage: 'Sorry there was an error sending your message.  Please try again later.' });
+    }
 
 
 
